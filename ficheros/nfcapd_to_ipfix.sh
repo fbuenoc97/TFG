79481723c4286010c9f025af9425408eac01125c
Felipe <root@192.168.1.109>
2022-05-24 19:04:47 +0200

    netflow to ipfix

--- ficheros/nfcapd_to_ipfix.sh
+++ ficheros/nfcapd_to_ipfix.sh
@@ -3,13 +3,13 @@
 RUTA=$1
 cd $RUTA
 for i in $(ls $PWD)
-	do
-	cd $i/
+	cd $i
 	for j in $(ls $PWD | grep .pcap)
 		do
 		nombre_fich=${j%.*}	
-		nfpcapd -r $j -l ../$i -t 60000
+		nfpcapd -r $j -l ../$i -t 10000000000000
 		mv nfcapd* $nombre_fich.ipfix	
 		done
 	cd ..
 	done
+
