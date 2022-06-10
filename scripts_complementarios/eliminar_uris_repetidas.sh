c0eefe4f6b0dc6fb85b9d70119ab74063d4e80df
Felipe <root@192.168.1.115>
2022-06-10 10:54:42 +0200

    short

--- scripts_complementarios/eliminar_uris_repetidas.sh
+++ scripts_complementarios/eliminar_uris_repetidas.sh
@@ -2,11 +2,18 @@
 
 RUTA=$1
 cd $RUTA
-for i in $(ls $PWD | grep .uri)
-	do	
-	    nombre_fich=${i%.*}
-            sort $i | uniq > $nombre_fich.tmp          # 1) Eliminar repeticiones 
-            LANG=C sort -f $nombre_fich.tmp > $nombre_fich.short       # 2) Ordenar alfanumericamente (ignore case)
-            rm -f $nombre_fich.tmp  2>&1 1>/dev/null  
-            wc -l $nombre_fich.short >> resumen.txt    # Contar URIs (líneas)
+
+for j in $(ls $PWD)
+	do
+	cd $j
+	echo $PWD
+	for i in $(ls $PWD | grep .uri)
+		do	
+		    nombre_fich=${i%.*}
+		    sort $i | uniq > $nombre_fich.tmp          # 1) Eliminar repeticiones 
+		    LANG=C sort -f $nombre_fich.tmp > $nombre_fich.short       # 2) Ordenar alfanumericamente (ignore case)
+		    rm -f $nombre_fich.tmp  2>&1 1>/dev/null  
+		   # wc -l $nombre_fich.short >> resumen.txt    # Contar URIs (líneas)
+		done
+	cd ../
 	done
